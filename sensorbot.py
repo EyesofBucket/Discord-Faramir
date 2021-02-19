69f986e2aa3d8cbc6d69f4e6be961479b3bd9b17
eyesofbucket <drufus6541@gmail.com>
2021-02-19 00:05:08 -0600

    Started building sensors

---
+++ sensorbot.py
@@ -0,0 +1,25 @@
+import os
+import discord
+from dotenv import load_dotenv
+import subprocess
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+SERVER = os.getenv('DISCORD_SERVER')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    for server in client.guilds:
+        if server.name == SERVER:
+            break
+    print('{client.user} has connected to Discord!\n\n{server.name}(id: {server.id})')
+
+@client.event
+async def on_message(message):
+    if message.content == '/sensors':
+        response = subprocess.run(['ls', '-l'], capture_output=True, text=True).stdout
+        await message.channel.send(response)
+
+client.run(TOKEN)
